45747bd6a8c4ba4d352cc5aa8cc1a8dd7b680557
maheshwaran-p <62535697+maheshwaran-p@users.noreply.github.com>
2021-03-24 16:38:27 +0530

    Create 10th.c

---
+++ 10th.c
@@ -0,0 +1,17 @@
+#include<stdint.h>
+
+#include<stdio.h>
+
+#include"bits.h"
+
+int main(int ac, char **av)
+
+{ uint8_t val=0b10101010; print_bits(val);
+
+putchar(’\n’); print_bits(get_subfield(val,0,2));
+
+putchar(’\n’); print_bits(get_subfield(val,1,3));
+
+putchar(’\n’); return 0;
+
+}
