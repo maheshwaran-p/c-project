db001da8c6f7ad52d2d38b0e010d335f6b73515e
maheshwaran-p <62535697+maheshwaran-p@users.noreply.github.com>
2021-03-24 16:37:48 +0530

    Create 9th.c

---
+++ 9th.c
@@ -0,0 +1,39 @@
+#include<stdint.h>
+
+#include<stdio.h>
+
+#include"bits.h"
+
+int main(int ac, char **av)
+
+{
+	
+	  uint8_t val=0; 
+	  uint8_t bit;
+	
+	  
+	  for (bit=0;bit<8;bit++){
+	  	printf("val: %u bit : %u ",val,bit);
+	  	
+	 print_bits(val);
+	  putchar(’\n’); 
+	  toggle_bit(&val,bit);
+	    print_bits(val);
+	  putchar(’\n’); 
+	  
+	  }
+	  
+	  	  for (bit=0;bit<8;bit++){
+	  	printf("val: %u bit : %u ",val,bit);
+	  	
+	 print_bits(val);
+	  putchar(’\n’); 
+	  toggle_bit(&val,bit);
+	    print_bits(val);
+	  putchar(’\n’); 
+	  
+	  }
+	  
+	    return 0;
+
+}
