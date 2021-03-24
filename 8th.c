ed23c8d692d48efda299d2dec4cac274787eb98f
maheshwaran-p <62535697+maheshwaran-p@users.noreply.github.com>
2021-03-24 16:37:10 +0530

    Create 8th.c

---
+++ 8th.c
@@ -0,0 +1,29 @@
+#include<stdint.h>
+
+#include<stdio.h>
+
+#include"bits.h"
+
+int main(int ac, char **av)
+
+{
+	
+	  uint8_t val=0b00000010; 
+	  uint8_t bit;
+	  print_bits(val);
+	  putchar(’\n’); 
+	  
+	  for (bit=0;bit<8;bit++){
+	  	printf("Bit %u is",bit);
+	  if(check_bit(val,bit)){
+	  	printf("on\n");
+	  	
+	  }
+	  else{
+	  	printf("off\n");
+	  }
+	  }
+	  
+	    return 0;
+
+}
